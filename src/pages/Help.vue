ecebb610573eb92c4122279a05eeb6ebada705bc
andres <53791842+AnrdesCoronado@users.noreply.github.com>
2020-05-03 21:41:26 -0500

    my first vue app

---
+++ src/pages/Help.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-page class="q-pa-lg">
+  <h5 class="q-mt-none">Help</h5>
+  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero soluta numquam nostrum laborum dignissimos expedita, repellat aliquam perferendis sapiente, incidunt ducimus. Modi asperiores obcaecati labore, magnam natus expedita hic eos!</p>
+  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. In a velit atque nesciunt quam, nisi nam incidunt non earum. Accusamus illo distinctio qui, molestias quasi, unde, dolores minima consectetur vero sunt illum cum similique ea non facilis? Libero eum fuga qui aliquam expedita reprehenderit corporis?</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+}
+</script>
---
+++ src/pages/Todo.vue
@@ -0,0 +1,47 @@
+<template>
+  <q-page class="bg-grey-3 column">
+    <q-list class="bg-white" separator bordered>
+      <q-item v-for="task in tasks" :key="task.title" @click="task.done =! task.done" v-ripple clickable>
+        <q-item-section avatar>
+          <q-checkbox v-model="task.done" color="primary" class="no-pointer-events"/>
+        </q-item-section>
+        <q-item-section v-if="task.done" >
+          <q-item-label>{{ task.title }}</q-item-label>
+        </q-item-section>
+        <q-item-section v-if="task.done" side >
+          <q-btn flat round color="primary" icon="delete" dense />
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </q-page>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      tasks: [
+        {
+          title: 'Get bannanas',
+          done: falsehttp://localhost:8080/#/
+        },
+        {
+          title: 'Eat bananas',
+          done: true
+        },
+        {
+          title: 'poo bananas',
+          done: false
+        }
+      ]
+    }
+  }
+}
+</script>
+<style lang="scss">
+  .done{
+    .q-item__label{
+      text-decoration: line-through;
+      color: #bbb;
+    }
+  }
+</style>
